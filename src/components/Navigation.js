4c4740bf08083d323c1494c69038e2e3a1fb9f05
José Espinoza <jose.espinoza.3d@gmail.com>
2024-04-29 22:51:45 -0500

    feat: implement links hover and refactor navigator

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,12 +3,16 @@ import { Link } from "react-router-dom";
 function Navigation() {
   return (
     <nav className="navigator">
-      <section className="navigator__options">
+      <section className="navigator__container">
         <h1 className="navigator__title">NewsExplorer</h1>
-        <Link className="navigator__link" to="/inicio">
-          Inicio
-        </Link>
-        <button className="button">Iniciar sesión</button>
+        <section className="navigator__options">
+          <section className="navigator__links">
+            <Link className="navigator__link" to="/inicio">
+              Inicio
+            </Link>
+          </section>
+          <button className="button">Iniciar sesión</button>
+        </section>
       </section>
       <div className="navigator__divider"></div>
     </nav>
