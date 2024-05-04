aeec27498e04ae2889307cc5c14804211a5d3a06
José Espinoza <jose.espinoza.3d@gmail.com>
2024-05-03 23:30:00 -0500

    feat: layout author section for desktop

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
+import About from "./components/About";
 
 function App() {
   return (
     <div className="page">
       <Header></Header>
       <Main></Main>
+      <About></About>
       <Footer></Footer>
     </div>
   );
