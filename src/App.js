3f1429e84f874cf703926774d99c00c28c5b8019
José Espinoza <jose.espinoza.3d@gmail.com>
2024-05-05 15:40:53 -0500

    feat: initial layout for main component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import "./App.css";
 import Header from "./components/Header";
-import Main from "./components/Main";
+import Main from "./components/Main/Main";
 import Footer from "./components/Footer";
 import About from "./components/About";
+
+import cards from "./data/cards.json";
 
 function App() {
   return (
     <div className="page">
       <Header></Header>
-      <Main></Main>
+      <Main cards={cards}></Main>
       <About></About>
       <Footer></Footer>
     </div>
