a94e2b00c1e40deddff9eb3b730d4c4a4ec92ce1
José Espinoza <jose.espinoza.3d@gmail.com>
2024-05-07 20:27:07 -0500

    feat: integrate application to third party news api

--- src/App.js
+++ src/App.js
@@ -3,13 +3,22 @@ import Header from "./components/Header";
 import Main from "./components/Main/Main";
 import Footer from "./components/Footer";
 import About from "./components/About";
-
 import cards from "./data/cards.json";
+import newsApi from "./utils/api";
+import { useState } from "react";
 
 function App() {
+  const [cards, setCards] = useState([]);
+
+  function handleNewsSearch(searchTerm) {
+    newsApi.getNews(searchTerm).then((result) => {
+      setCards(result.articles);
+    });
+  }
+
   return (
     <div className="page">
-      <Header></Header>
+      <Header onSearch={handleNewsSearch}></Header>
       <Main cards={cards}></Main>
       <About></About>
       <Footer></Footer>
