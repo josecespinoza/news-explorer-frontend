9bd6cc79ac5ddbcb867d903822529a30e802aa7c
José Espinoza <jose.espinoza.3d@gmail.com>
2024-05-19 17:00:13 -0500

    feat: add save article button

--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -1,4 +1,5 @@
 import "./NewsCard.css";
+import bookmark from "../../images/bookmark.svg";
 
 function NewsCard({ card }) {
   const publishedAt = new Date(card.publishedAt).toLocaleString("es-PE", {
@@ -7,8 +8,22 @@ function NewsCard({ card }) {
     year: "numeric",
   });
 
+  function handleSaveCardClick() {}
+
   return (
     <li className="news-card">
+      <button
+        className="button news-card__button"
+        onClick={handleSaveCardClick}
+      >
+        <div className="button__content">
+          <img
+            className="button__icon"
+            alt="bookmark icon"
+            src={bookmark}
+          ></img>
+        </div>
+      </button>
       <img src={card.urlToImage} alt="" className="news-card__photo" />
       <section className="news-card__content">
         <h4 className="news-card__date">{publishedAt}</h4>
