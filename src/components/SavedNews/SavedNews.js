e372536c3e386b6de57f742587e4359d69e81b37
José Espinoza <jose.espinoza.3d@gmail.com>
2024-05-22 23:05:10 -0500

    feat: show how many articles are saved and related keywords

--- src/components/SavedNews/SavedNews.js
+++ src/components/SavedNews/SavedNews.js
@@ -3,19 +3,31 @@ import NewsCardList from "../NewsCardList/NewsCardList";
 import "./SavedNews.css";
 
 function SavedNews({ savedCards }) {
+  function getUniqueKeywords(cards) {
+    const keywords = cards.map(
+      (card) =>
+        card.keyword.substring(0, 1).toUpperCase() +
+        card.keyword.substring(1, card.keyword.length)
+    );
+    const uniqueKeywords = [...new Set(keywords)];
+    return `${[...uniqueKeywords].slice(0, 2).join(", ")} y ${
+      uniqueKeywords.length
+    } más`;
+  }
+
   return (
     <main className="main">
       <section className="saved-news">
         <section className="saved-news__header">
           <h6 className="saved-news__section-title">Artículos guardados</h6>
           <h3 className="saved-news__title">
-            {`Elise, tienes 5 artículos 
+            {`Elise, tienes ${savedCards.length} artículos 
             guardados`}
           </h3>
           <h5 className="saved-news__legend">
             Por palabras clave:{" "}
             <span className="saved-news__legend_weight_bold">
-              Naturaleza, Yellowstone, y 2 más
+              {getUniqueKeywords(savedCards)}
             </span>
           </h5>
         </section>
